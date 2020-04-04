d4ff50b5b2921f6d36f69a7c2946cdad103172eb
Ajay <majay1638@gmail.com>
2020-04-04 13:08:28 +0530

    app intialize

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 
 //Intial Route
 app.get('/', (req, res) => {
-    res.send({ msg: 'You are ready to rock bro....' });
+    res.send({ msg: 'You are ready to rock' });
 })
 
 
