2590dc3bfc9b3f42ae196bc7e406373054c5beff
Ajay <majay1638@gmail.com>
2020-04-04 13:05:46 +0530

    app intialize

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>You are reday to make an awesome app => <p style={{ color: 'blue' }}>Start Editing</p> </h1>
+    </div>
+  )
+}
+
+export default App;
